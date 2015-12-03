a911194b8583a7b23004fbf8140d24fabf51e29e
BuildTools <unconfigured@null.spigotmc.org>
2015-12-03 17:03:31 +0100

    added buttons

---
+++ core/src/com/rpgproject/view/buttons/MainMenuButton.java
@@ -0,0 +1,14 @@
+package com.rpgproject.view.buttons;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
+/**
+ * Created by lukas on 3-12-2015.
+ */
+public class MainMenuButton extends TextButton{
+
+    public MainMenuButton(String text, Skin skin) {
+        super(text, skin);
+    }
+}
